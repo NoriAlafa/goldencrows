12c638d546ddfefcfb67eeda95d5d208a6846285
NoriAlafa <alafanori@gmail.com>
2022-04-28 09:19:28 +0700

    [EDIT]blog server side

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import '../styles/Home-module.css'
 import '../styles/User-module.css'
+import '../styles/Blog-module.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,28 @@
 import Layout from "../components/layout";
 
-export default function blog(){
+export default function Blog(props){
+    const {dataBlog} = props
+
     return(
         <Layout pageTitle="Blog">
-            <div>
-                <p>Blog</p>
-            </div>
+            {dataBlog.map(blog=>{
+                return(
+                    <div key={blog.id} className="card-blog">
+                        <h3>{blog.title}</h3>
+                        <p>{blog.body}</p>
+                    </div>
+                )
+            })}
         </Layout>
     )
+}
+
+export async function getServerSideProps(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const dataBlog = await res.json()
+    return{
+        props:{
+            dataBlog,
+        }
+    }
 }
